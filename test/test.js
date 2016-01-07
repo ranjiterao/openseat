673961579f7cfe05dac44692e2f5bcf911f7c90a
Max Kahn <kahn.max@gmail.com>
2016-01-06 20:36:26 -0800

    Started writing tests.

---
+++ test/test.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+
+//example of using Mocha, just to make sure the testing suite works
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2 ,3].indexOf(5));
+      assert.equal(-1, [1, 2,3].indexOf(0));
+    });
+  });
+});
+
+describe('utility', function() {
+  before(function() {
+    mockery.enable();
+    mockery.registerAllowable('spherical');
+  });
+
+  it('should compute the distance between two sets of coordinates', function() {
+
+  });
+
+  it('should throw an error if the input is not in the correct format', function() {
+
+  });
+
+  it('should return an object of type number', function() {
+
+  });
+
+  it('should be accessible from other Node modules', function() {
+
+  });
+
+  after(function() {
+
+  });
+})
