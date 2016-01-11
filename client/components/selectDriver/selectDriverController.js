3de071c6b2dbec9a7b719849bedfcaec88c0d3c0
Max Kahn <kahn.max@gmail.com>
2016-01-11 09:57:44 -0800

    Manual re-add of dynamically generated maps.

--- client/components/selectDriver/selectDriverController.js
+++ client/components/selectDriver/selectDriverController.js
@@ -1,20 +1,25 @@
-angular.module('selectDriverModule', [])
-  .controller('selectDriverCtrl', function($scope, $rootScope, $routeParams, Routes){
+angular.module('selectDriverModule', ['ngMap'])
+  .controller('selectDriverCtrl', function($scope, $rootScope, $routeParams, Routes, ngMap){
     var passengerRouteId = $routeParams.id;
     $scope.driverRoutes = {};
 
-    var initializeDriverRoutes = function(passengerRouteId){
-      Routes.bestDriverRoutesForPassengerRouteId(passengerRouteId, function(driverRoutes){
-        $scope.driverRoutes = driverRoutes;
-      });
-    };
 
-    if (passengerRouteId) {
-      initializeDriverRoutes(passengerRouteId);
-    }
+    NgMap.getMap().then(function (map) {
+      $scope.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
 
-    $scope.selectDriverRoute = function(driverRouteId){
+      var initializeDriverRoutes = function(passengerRouteId){
+        Routes.bestDriverRoutesForPassengerRouteId(passengerRouteId, function(driverRoutes){
+          $scope.driverRoutes = driverRoutes;
+        });
+      };
+
+      if (passengerRouteId) {
+        initializeDriverRoutes(passengerRouteId);
+      }
+
+      $scope.selectDriverRoute = function(driverRouteId){
       Routes.userInterestedInDriverRoute(passengerRouteId, driverRouteId);
       //TODO show something in the view that confirms the driverRoute was added
-    };
+      };
+    });
   });
