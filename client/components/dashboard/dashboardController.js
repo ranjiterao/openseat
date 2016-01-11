e8ace0ec356cbd91165220b64ecea9c193b2eccd
Eric Geniesse <egeniesse@gmail.com>
2016-01-11 14:06:39 -0800

    finished the styling for the dashboard

--- client/components/dashboard/dashboardController.js
+++ client/components/dashboard/dashboardController.js
@@ -3,18 +3,24 @@ angular.module('dashboardModule', [])
 .controller('renderUserCtrl', function($scope, RenderUser, $rootScope, Routes) {
 
 
-// This function takes an array of routes and iterates over it replacing the array of booleans
-// that represend the days and returns a string with the requested days.
+	// This function takes an array of routes and iterates over it replacing the array of booleans
+	// that represend the days and returns a string with the requested days.
 
-	var setDays = function(arrayOfRoutes) {
+	var setDays = function(arrayOfRoutes, isDriver) {
 		var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 		for (var i = 0; i < arrayOfRoutes.length; i++) {
-			arrayOfRoutes[i].days = arrayOfRoutes[i].days.reduce(function(memo, day, index) {
-				if (day) {
-					memo = memo.concat(days[index] + ' ');
-				}
-				return memo;
-			}, '');
+			if (isDriver) {
+				setDays(arrayOfRoutes[i].confirmedPassengerRoutes);
+				setDays(arrayOfRoutes[i].prospectivePassengerRoutes);
+			}
+			if (arrayOfRoutes[0]) {
+				arrayOfRoutes[i].days = arrayOfRoutes[i].days.reduce(function(memo, day, index) {
+					if (day) {
+						memo = memo.concat(days[index] + ' ');
+					}
+					return memo;
+				}, '');
+			}
 		}
 		return arrayOfRoutes;
 	};
@@ -27,17 +33,23 @@ angular.module('dashboardModule', [])
 
 	var initializeDriverRoutes = function(driverId) {
 		Routes.getDriverRoutesForUserId(driverId, function(driverRoutes) {
-			$scope.driverRoutes = setDays(driverRoutes);
+			$scope.driverRoutes = setDays(driverRoutes, true);
 		});
 	};
 
 	$scope.deleteConfirmedPassenger = function(passengerRouteId) {};
 
 	$scope.confirmPassenger = function(passengerRouteId, driverRouteId) {
+		var currArr = this.$parent.route.prospectivePassengerRoutes;
+		if (currArr[i]._id === passengerRouteId) {
+			var addedRoute = this.$parent.route.prospectivePassengerRoutes.splice(i, 1);
+			this.$parent.route.confirmedPassengerRoutes.push(addedRoute);
+		}
 		Routes.driverConfirmsPassenger(driverRouteId, passengerRouteId, function() {
-			//TODO
+			initializeDriverRoutes($rootScope.user._id);
 		});
 	};
+
 
 	$scope.username = 'Hello ' + $rootScope.user.name;
 	if ($rootScope.user.IsDriver) {
