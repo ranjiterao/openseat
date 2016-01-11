467812db124acfe9f6b7ac542263171579d5395f
Ranjit Rao <ranjiterao@gmail.com>
2016-01-11 14:20:32 -0800

    merge conflict part 2 addressed

--- server/components/routes/driverRoutesModel.js
+++ server/components/routes/driverRoutesModel.js
@@ -13,16 +13,13 @@ var DriverRoutesSchema = new mongoose.Schema({
   fromMinutes: {min: 0, max: 60, type: Number},
   toHour: {min: 0, max: 24, type: Number},
   toMinutes: {min: 0, max: 60, type: Number},
-  endLabel: String,
   name: String,
-  startLabel: String,
   seats: Number,
   fee: Number,
 
   prospectivePassengerRoutes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'passengerroutes' }],
   confirmedPassengerRoutes: [{type: mongoose.Schema.Types.ObjectId, ref: 'passengerroutes'}],
   driverInformation: {type: mongoose.Schema.Types.ObjectId, ref: 'users'}
-
 });
 
 module.exports = mongoose.model('driverRoutes', DriverRoutesSchema);
