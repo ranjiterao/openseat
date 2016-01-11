3da182d0575b87ff335089986b0ace190c61b62c
Eric Geniesse <egeniesse@gmail.com>
2016-01-11 09:45:22 -0800

    Finished rendering the days instead of the array of booleans

--- client/components/dashboard/dashboardController.js
+++ client/components/dashboard/dashboardController.js
@@ -1,32 +1,46 @@
 angular.module('dashboardModule', [])
 
-.controller('renderUserCtrl', function($scope, RenderUser, $rootScope, Routes){
-	$scope.passengerRoutes = {};
-	$scope.driverRoutes = {};
+.controller('renderUserCtrl', function($scope, RenderUser, $rootScope, Routes) {
 
-	var initializePassengerRoutes = function(passengerId){
-		Routes.getPassengerRoutesForUserId(passengerId, function(passengerRoutes){
-			$scope.passengerRoutes = passengerRoutes;
+
+// This function takes an array of routes and iterates over it replacing the array of booleans
+// that represend the days and returns a string with the requested days.
+
+	var setDays = function(arrayOfRoutes) {
+		var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+		for (var i = 0; i < arrayOfRoutes.length; i++) {
+			arrayOfRoutes[i].days = arrayOfRoutes[i].days.reduce(function(memo, day, index) {
+				if (day) {
+					memo = memo.concat(days[index] + ' ');
+				}
+				return memo;
+			}, '');
+		}
+		return arrayOfRoutes;
+	};
+
+	var initializePassengerRoutes = function(passengerId) {
+		Routes.getPassengerRoutesForUserId(passengerId, function(passengerRoutes) {
+			$scope.passengerRoutes = setDays(passengerRoutes);
 		});
 	};
 
-	var initializeDriverRoutes = function(driverId){
-		Routes.getDriverRoutesForUserId(driverId, function(driverRoutes){
-			$scope.driverRoutes = driverRoutes;
+	var initializeDriverRoutes = function(driverId) {
+		Routes.getDriverRoutesForUserId(driverId, function(driverRoutes) {
+			$scope.driverRoutes = setDays(driverRoutes);
 		});
 	};
 
-	$scope.deleteConfirmedPassenger = function(passengerRouteId){
-	};
+	$scope.deleteConfirmedPassenger = function(passengerRouteId) {};
 
-	$scope.confirmPassenger = function(passengerRouteId, driverRouteId){
-		Routes.driverConfirmsPassenger(driverRouteId, passengerRouteId, function(){
+	$scope.confirmPassenger = function(passengerRouteId, driverRouteId) {
+		Routes.driverConfirmsPassenger(driverRouteId, passengerRouteId, function() {
 			//TODO
 		});
 	};
-	
+
 	$scope.username = 'Hello ' + $rootScope.user.name;
-	if ($rootScope.user.IsDriver){
+	if ($rootScope.user.IsDriver) {
 		initializeDriverRoutes($rootScope.user._id);
 	} else {
 		initializePassengerRoutes($rootScope.user._id);
